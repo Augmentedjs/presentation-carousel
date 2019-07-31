a65bd42be7dde37fea670a6f5399e73b6dc21d9d
thedocbwarren <jedi.master@augmentedjs.com>
2019-07-30 20:38:52 -0700

    add setter for value

--- src/imageCarouselView.js
+++ src/imageCarouselView.js
@@ -30,32 +30,39 @@ class ImageCarouselView extends DirectiveView {
         </div>
       `;
       this._images = options.images;
-      this._value = this._images[this._location].value;
+      this.value = this._images[this._location].value;
     } else {
-      this.template = /*html*/
-        `<div class="carousel"><p>Empty</p></div>`;
-        this._images = [];
-        this._value = null;
+      this.template = /*html*/`<div class="carousel"><p>Empty</p></div>`;
+      this._images = [];
+      this.value = null;
     }
+  };
+
+  set value(value) {
+    return this.model.set(this.name, value);
+  };
+
+  get value() {
+    return this.model.get(this.name);
   };
 
   _changePosition(x) {
     if (this._carousel && this._firstimg) {
       const size = this._firstimg.offsetWidth;
       const trans = (this._location * size) / 16;
-      console.debug(size, trans);
-      console.debug(this._carousel);
+      //console.debug(size, trans);
+      //console.debug(this._carousel);
 
       this._carousel.style.transform = `translateX(-${trans}rem)`;
-      this._value = this._images[this._location].value;
+      this.value = this._images[this._location].value;
 
-      console.debug("value", this._value);
+      //console.debug("value", this._value);
     }
   };
 
   left(e) {
     e.preventDefault();
-    console.debug("left", this._location, this._images.length);
+    //console.debug("left", this._location, this._images.length);
     if (this._location !== 0) {
       // const carousel = document.querySelector(`${this.el} > div.carousel > div`);
       // const img = document.querySelector(`${this.el} > div.carousel > div > figure`);
@@ -69,7 +76,7 @@ class ImageCarouselView extends DirectiveView {
 
   right(e) {
     e.preventDefault();
-    console.debug("right", this._location, this._images.length);
+    //console.debug("right", this._location, this._images.length);
     if (this._location !== this._images.length - 1) {
       // const carousel = document.querySelector(`${this.el} > div.carousel > div`);
       // const img = document.querySelector(`${this.el} > div.carousel > div > figure`);
@@ -79,10 +86,6 @@ class ImageCarouselView extends DirectiveView {
       }
     }
     return false;
-  };
-
-  get value() {
-    return this._value;
   };
 
   async render() {
